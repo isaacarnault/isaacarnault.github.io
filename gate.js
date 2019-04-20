6530f36861b0be8da6fdeda59185048fe6b29b2c
Isaac Arnault <44641020+isaacarnault@users.noreply.github.com>
2019-04-20 14:06:19 +0200

    Update gate.js

--- gate.js
+++ gate.js
@@ -1,28 +1,26 @@
-<script language="javascript">
-<!--//
-/*This Script allows people to enter by using a form that asks for a
-UserID and Password*/
-function pasuser(form) {
-if (form.id.value=="JavaScript") { 
-if (form.pass.value=="Kit") {              
-location="page2.html" 
-} else {
-alert("Invalid Password")
+<SCRIPT>
+function passWord() {
+var testV = 1;
+var pass1 = prompt('Please Enter Your Password',' ');
+while (testV < 3) {
+if (!pass1)
+history.go(-1);
+if (pass1.toLowerCase() == "letmein") {
+alert('You Got it Right!');
+window.open('protectpage.html');
+break;
 }
-} else {  alert("Invalid UserID")
+testV+=1;
+var pass1 =
+prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
 }
+if (pass1.toLowerCase()!="password" & testV ==3)
+history.go(-1);
+return " ";
 }
-//-->
-</script>
-
-<center>
-<table bgcolor="white" cellpadding="12" border="1">
-<tr><td colspan="2"><center><h1><i><b>Login
-Area</b></i></h1></center></td></tr>
-<tr><td><h1><i><b>UserID:</b></i></h1></td><td><form name="login"><input
-name="id" type="text"></td></tr>
-<tr><td><h1><i><b>Password:</b></i></h1></td><td><input name="pass"
-type="password"></td></tr>
-<tr><td><center><input type="button" value="Login"
-onClick="pasuser(this.form)"></center></td><td><center><br><input
-type="Reset"></form></td></tr></table></center> 
+</SCRIPT>
+<CENTER>
+<FORM>
+<input type="button" value="Enter Protected Area" onClick="passWord()">
+</FORM>
+</CENTER>
